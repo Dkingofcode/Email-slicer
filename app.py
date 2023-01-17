84d0a5de082bf37a6f40303a07f5ddb9eb63091c
Dkingofcode <doladepo128@gmail.com>
2023-01-17 10:36:22 +0000

    Creating the input variable

---
+++ app.py
@@ -0,0 +1,22 @@
+# collect email from user
+# slice/ split the email using the @
+#  save the first part as the user and the second part as the domain name
+
+def main():
+    print(" Welcome to the email slicer ")
+    print("")
+
+    email_input = input("Input your email address:") 
+
+
+
+
+
+
+
+
+
+
+
+
+
